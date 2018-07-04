a23d7318ae980a5cee3dca361d2bd6c9c40aaf67
Ganesh pandian <ganeshh777@gmail.com>
2018-07-04 11:29:32 +0530

    Not operator fixed

--- src/main/java/wasdev/compoundconcept/ibmclient/IBMQueryClient.java
+++ src/main/java/wasdev/compoundconcept/ibmclient/IBMQueryClient.java
@@ -5,6 +5,7 @@ import java.net.URLEncoder;
 import com.ibm.watson.developer_cloud.discovery.v1.Discovery;
 import com.ibm.watson.developer_cloud.discovery.v1.model.QueryOptions;
 import com.ibm.watson.developer_cloud.discovery.v1.model.QueryResponse;
+import com.ibm.watson.developer_cloud.service.exception.BadRequestException;
 
 public class IBMQueryClient {
 	public QueryResponse query(long resultLimit,String queryString) throws Exception {
@@ -13,17 +14,31 @@ public class IBMQueryClient {
 		String environmentId = "dde1b791-3e20-4b60-9f7f-9ced24a4bc31";
 		String collectionId = "436ba503-4708-474d-ae39-3b54af21ab0d";
 
-		QueryOptions.Builder queryBuilder = new QueryOptions.Builder(environmentId, collectionId);
-		queryBuilder.query(mapToIBMQuery(queryString));
-		queryBuilder.highlight(true);
-		queryBuilder.count(resultLimit);
+		QueryOptions.Builder queryBuilder = null;
+		QueryResponse queryResponse= null;
 		
-		QueryResponse queryResponse = discovery.query(queryBuilder.build()).execute();
+		try {
+			queryBuilder = new QueryOptions.Builder(environmentId, collectionId);
+			queryBuilder.query(mapToIBMQueryV2(queryString));
+			queryBuilder.highlight(true);
+			queryBuilder.count(resultLimit);
+			
+			queryResponse = discovery.query(queryBuilder.build()).execute();
+		}catch(BadRequestException e) {
+			System.out.println("V2 Exception..FallBack V1");
+			queryBuilder = new QueryOptions.Builder(environmentId, collectionId);
+			queryBuilder.query(mapToIBMQueryV1(queryString));
+			queryBuilder.highlight(true);
+			queryBuilder.count(resultLimit);
+			
+			queryResponse = discovery.query(queryBuilder.build()).execute();
+		}
+		
 		System.out.println("queryStr = "+queryString+ "  Response = "+queryResponse);
 		return queryResponse;
 	}
 	
-	public String mapToIBMQuery(String rawQuery) {
+	public String mapToIBMQueryV1(String rawQuery) {
 		System.out.print(rawQuery);
 		String ibmQueryFormat = null;
 		
@@ -40,6 +55,34 @@ public class IBMQueryClient {
 							.replaceAll("(?i)title", "TITLE")
 							.replaceAll("(?i)content", "CONTENT")
 							.replaceAll("(?i)Organizations", "enriched_CONTENT.entities.relevance>0.8,enriched_CONTENT.entities.type::\"Organization\",enriched_CONTENT.entities.text");
+		}
+		
+		System.out.println("IBM query : "+ibmQueryFormat);
+		return ibmQueryFormat;
+	}
+	
+	public String mapToIBMQueryV2(String rawQuery) {
+		System.out.print(rawQuery);
+		String ibmQueryFormat = null;
+		
+		if(rawQuery.indexOf("Count") >= 0 ) {
+			ibmQueryFormat = handleCountOperator(rawQuery);
+		}
+		else {
+			ibmQueryFormat = rawQuery.replaceAll("AND", ",")
+							.replaceAll("OR", "|")
+							.replaceAll("\\*","")
+							.replaceAll("(?i)NOT title :", "TITLE :!")
+							.replaceAll("(?i)NOT content : ", "CONTENT :!")
+							.replaceAll("(?i)title :", "TITLE :")
+							.replaceAll("(?i)content : ", "CONTENT :")
+							.replaceAll("(?i)Concepts", "Concept")
+							.replaceAll("(?i)NOT concept :", "enriched_CONTENT.concepts.text : !")
+							.replaceAll("(?i)Concept :", "enriched_CONTENT.concepts.text :")
+							.replaceAll("(?i)NOT Locations :", "enriched_CONTENT.entities.relevance>0.8, enriched_CONTENT.entities.type::\"Location\", enriched_CONTENT.entities.text :!")
+							.replaceAll("(?i)Locations :", "enriched_CONTENT.entities.relevance>0.8, enriched_CONTENT.entities.type::\"Location\", enriched_CONTENT.entities.text :")
+							.replaceAll("(?i)NOT Organizations :", "enriched_CONTENT.entities.relevance>0.8,enriched_CONTENT.entities.type::\"Organization\",enriched_CONTENT.entities.text:!")	
+							.replaceAll("(?i)Organizations :", "enriched_CONTENT.entities.relevance>0.8,enriched_CONTENT.entities.type::\"Organization\",enriched_CONTENT.entities.text:");
 		}
 		
 		System.out.println("IBM query : "+ibmQueryFormat);
